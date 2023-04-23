dea3699e169a922997ea3a9c02a64cddffbffbc1
Vic-Lu <128205426+Vic-Lu@users.noreply.github.com>
2023-04-23 14:24:26 -0700

    first update
    componentes iniciales del proyecto. ui, code. assets

---
+++ Assets/Scenes/GameStart.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+
+public class GameStart : MonoBehaviour
+{
+   
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool pressBotton;
+
+
+      
+        { 
+
+        }
+    }
+
+    // easy questions 
+    // 1  ¿ donde se encuentra la tierra?
+    // 2 ¿ cuales son los colores primarios?
+    // 3  ¿ donde se paro el aguila?
+    // 4 ¿ cual de los siguientes no sea emplea en la cocina ?
+    // 5 ¿ de que color es la sangre ?
+    // 6 ¿ que significa el termino ¨Hate¨ ?
+    // 7 ¿ algo que sea una obviedad?
+    // 8 ¿ quien se robo el caballo blanco de napoleon ?
+    // 9 ¿ que no es cripto ?
+    // 10 ¿ cual de estas no es un clasico de navidad?
+
+    //  ________________________________________________________________
+
+    // normal questions 
+    // 1  ¿ cual es la raiz cuadrada de 64 ?
+    // 2 ¿ que significa  ¨Je suis¨ ?
+    // 3 ¿ a quien suelen llamar ¨el planeta azul¨  ?
+    // 4 ¿ donde se encuentra zanzibar ?
+    // 5 ¿ que simbolo es utilizado normalmente para hacer campañas ?
+    // 6 ¿ juego de street fighter donde los ataques se realizan acomulando gemas de colores?
+    // 7 ¿ que se encuentra a un lado del ketchup, atras de los pepinillos, a la izquierda de la ensalada de col ?
+    // 8 ¿ a quien se refiere la frase  ¨... is a Lie ¨ ?
+    // 9 ¿ cual de los siguientes personajes es mejor?
+    // 10 ¿ como termina la frase:  ¨ go woke ... ¨ ? 
+
+    //  ________________________________________________________________
+
+    //  Class preguntas  Hard:
+    // 1 ¿ a quien refiere el termino ¨Pipero¨ ?
+    // 2 ¿ de que habla la cancion de Ramstein ¨Links¨ ?
+    // 3 ¿ cual es la mejor forma de eliminar a los Undead en final Fantasy?
+    // 4 ¿ cual fue la consola portatil de Bandai?
+    // 5  ¿ que significa 0010100 1010111  ?
+    // 6 ¿ cual seria la equivalencia 3D a los pixels?
+    // 7 ¿ a que se debe el famoso efecto de neblina en Silent hill ?
+    // 8 ¿ que significa el termino ¨DirectX¨ ?
+    // 9 ¿  a quien hace referencia el slogan ¨ Power yours Dreams ¨?
+    // 10 ¿ que mato a Sega ?
+
+    // ==================================================================
+
+    // easy answers
+    // 1   en el patio ,  el espacio, el sistema solar(*) , una galaxia muy lejana
+    // 2  cafe-morado-naranja , amarillo-azul-rojo(*),  verde-rosa-azul, verde-azul-rojo
+    // 3  en su nido , una montaña, un poste, un nopal(*)
+    // 4  cloroformo(*) , anis , petalos, curcuma
+    // 5  negra, azul, roja(*), morada 
+    // 6 un hocho(*), odio(*), una moda, tecnicsismo
+    // 7  el valor de ¨X¨  , el valor de dinero, el agua moja, que no te quiere(*)
+    // 8 obvio tu, el brayan, ¿cual caballo?(*), blanco   
+    // 9 comida(*), un perro, una moneda, ET
+    // 10  un cuento de navidad, the thing, gremlings, home alone
+
+    //  _________________________________________________________________
+
+    // normal answers
+
+    // 1  3 ,  0 ,  6 , 8(*)
+    // 2 no sé, yo soy(*) , jesus, un insulto
+    // 3 la tierra(*), pandora, io , mon kalamari
+    // 4 una isla, oceania, africa(*) , mediterraneo
+    // 5 #(*) , @ , $ , &
+    // 6 SSFIIT , SFIII3S, Tekken vs SF, SPF(*)
+    // 7 el nirvana, un queque de frutas(*) , tu dignidad , un portal
+    // 8  the cake(*) , House, everybody , Life 
+    // 9 pikachu, mario, cheems(*), spiderman
+    // 10 go home , go broke(*) , rules ! , up ! 
+
+    // ___________________________________________________________________
+
+    // Class respestas Hard
+    // 1 conductor de pipas, quien come pipas, groupies de PS(*) , un idiota
+    // 2 una aventura de link, un sitio web, hormigas, una marcha(*)
+    // 3 phoenix down(*) , cure , run , fire  
+    // 4 gameboy  , wonderswan(*) , PSP, ncage
+    // 5 101011 , numeros , codigo , S117(*)
+    // 6 poligonos, voxels(*) , shadders, renders
+    // 7 decision artistica, crush, limitaciones tecnicas(*) , raytracing
+    // 8 API(*) , nopor , charles xavier, una pieza de hardware
+    // 9 dreamcast , colchones , pildoras para dormir, xbox series(*)
+    // 10 Roger rabbit, Dreamcast, malas deciciones(*) , pirateria
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
