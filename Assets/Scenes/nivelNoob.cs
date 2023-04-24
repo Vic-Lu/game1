a8755424e1a60e4783797a78db074f21c0faec90
Vic-Lu <128205426+Vic-Lu@users.noreply.github.com>
2023-04-23 19:34:33 -0700

    arreglo preguntas
    se configuro el arreglo para contener las preguntas y sus respuestas

--- Assets/Scenes/nivelNoob.cs
+++ Assets/Scenes/nivelNoob.cs
@@ -7,10 +7,22 @@ public class nivelNoob : MonoBehaviour
 {
     // elemento para class nivNoob
 
+    GameObject[] aregloQuestion = new GameObject[10];
     int playerScore;
     public TextMeshProUGUI Score;
     bool correctAnswer;
+    int questionNumber;
 
+    public GameObject Q1;
+    public GameObject Q2;
+    public GameObject Q3;
+    public GameObject Q4;
+    public GameObject Q5;
+    public GameObject Q6;
+    public GameObject Q7;
+    public GameObject Q8;
+    public GameObject Q9;
+    public GameObject Q10;
     void rewriteTextScore(int puntos)
     {
         Score.SetText(puntos.ToString() + " puntos");
@@ -21,7 +33,22 @@ public class nivelNoob : MonoBehaviour
     {
         playerScore = 0;
         rewriteTextScore(playerScore);
+        Debug.Log(Q1);
 
+        // Q1.SetActive(true);
+    
+        aregloQuestion[0] = Q1;
+        aregloQuestion[1] = Q2;
+        aregloQuestion[2] = Q3;
+        aregloQuestion[3] = Q4;
+        aregloQuestion[4] = Q5;
+        aregloQuestion[5] = Q6;
+        aregloQuestion[6] = Q7;
+        aregloQuestion[7] = Q8;
+        aregloQuestion[8] = Q9;
+        aregloQuestion[9] = Q10;
+
+        aregloQuestion[0].SetActive(true); 
     }
 
     // Update is called once per frame
