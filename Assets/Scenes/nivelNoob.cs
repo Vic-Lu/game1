a88d91bbfab77ef82da0f293a061322d675c1f73
Vic-Lu <128205426+Vic-Lu@users.noreply.github.com>
2023-04-23 17:31:14 -0700

    funcion para llevar score
    asignacion inicial parametros, para que el contador de puntos del jugador, empieze en 0

--- Assets/Scenes/nivelNoob.cs
+++ Assets/Scenes/nivelNoob.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class nivelNoob : MonoBehaviour
 {
+    // elemento para class nivNoob
+
+    int playerScore;
+    public TextMeshProUGUI Score;
+    bool correctAnswer;
+
+    void rewriteTextScore(int puntos)
+    {
+        Score.SetText(puntos.ToString() + " puntos");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerScore = 0;
+        rewriteTextScore(playerScore);
+
     }
 
     // Update is called once per frame
